03e42896838a33ae46ad1c945094637ad269bb45
Souvi <souvima@hotmail.com>
2023-09-11 22:15:38 +0200

    Add assets to files js

---
+++ my-app/src/components/Images.js
@@ -0,0 +1,7 @@
+import ArgentBankLogo from "../assets/argentBankLogo.png";
+import BankTree from "../assets/bank-tree.jpeg";
+import IconChat from "../assets/icon-chat.png";
+import IconMoney from "../assets/icon-money.png";
+import IconSecurity from "../assets/icon-security.png";
+
+export { ArgentBankLogo, BankTree, IconChat, IconMoney, IconSecurity };
